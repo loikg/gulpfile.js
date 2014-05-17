a07d3615936e2981f8902d110e6ba3d86bd2d31f
loikg <lolo75017@gmail.com>
2014-05-17 22:28:24 +0200

    rewrite

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,162 @@
-/**
-*   Mon fichier gulp [24/04/2014]:
-**/
+/*-------------------------------------------------------------------
+    Required plugins
+-------------------------------------------------------------------*/
+var    gulp = require('gulp'),
+        compass = require('gulp-compass'),
+        minifyCss = require('gulp-minify-css'),
+        coffee = require('gulp-coffee'),
+        uglify = require('gulp-uglify'),
+        plumber = require('gulp-plumber'),
+        imagemin = require('gulp-imagemin'),
+        pngcrush = require('imagemin-pngcrush'),
+        livereload = require('gulp-livereload'),
+        filter = require('gulp-filter'),
+        clean = require('gulp-clean'),
+        zip = require('gulp-zip'),
+        ftp = require('gulp-ftp'),
+        sftp = require('gulp-sftp'),
+        useref = require('gulp-useref');
 
-/*----------------------------------------------------------------------------
-*   Initialisation des plugins utlisés
-*---------------------------------------------------------------------------*/
-var gulp = require('gulp'),
-    coffee = require('gulp-coffee'),
-    compass = require('gulp-compass'),
-    livereload = require('gulp-livereload'),
-    minifyCSS = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    prefix = require('gulp-autoprefixer'),
-    minifyCSS = require('gulp-minify-css');
+var    jsFilter = filter('**/*.js'),
+        cssFilter = filter('**/*.css');
 
-/*----------------------------------------------------------------------------
-*   Configuration paths
-*---------------------------------------------------------------------------*/
-var paths = {
-    dev : {
-        scss : 'public/dev/css/**/*.scss',
-        coffee :'public/dev/js/**/*.coffee',
-        img : 'public/dev/img'
-    },
-    dist : {
-        js :'public/assets/js',
-        css :'public/assets/css',
-        img : 'public/assets/img'
-    }
+/*-------------------------------------------------------------------
+    Configuration
+-------------------------------------------------------------------*/
+path = {
+    app: "app",
+    scss: "app/css",
+    css: "app/css",
+    coffee: "app/js",
+    js: "app/js",
+    img: "app/img"
 }
 
+watched = {
+    scss: path.scss + '/**/*.scss',
+    coffee: path.coffee + '/**/*.coffee',
+    html: path.app + '/**/*.html',
+    img: path.img + '/**/*',
+    all: path.app + '/**/*.*'
+}
 
+deploy = {
+    host : "",
+    user: "",
+    pass: ""
+}
 
+/*------DEV TASKS------*/
 
-
-/*----------------------------------------------------------------------------
-*   Task compilation du coffeeScript
-*---------------------------------------------------------------------------*/
-gulp.task('coffee', function(){
-    return gulp.src('public/dev/js/**/*.coffee')
-        .pipe(coffee())
-        //.pipe(uglify({outSourceMap: true}))
-        .pipe(gulp.dest('public/assets/js'));
+/*-------------------------------------------------------------------
+    Coffee Compilation
+-------------------------------------------------------------------*/
+gulp.task('coffee', function() {
+  return gulp.src(watched.coffee)
+            .pipe(plumber())
+            .pipe(coffee({bare: true}))
+            .pipe(gulp.dest(path.js));
 });
 
-/*----------------------------------------------------------------------------
-*   Task compilation du js
-*---------------------------------------------------------------------------*/
-gulp.task('js', function(){
-    return gulp.src('public/dev/js/**/*.js')
-        .pipe(uglify({outSourceMap: true}))
-        .pipe(gulp.dest('public/assets/js'));
+/*-------------------------------------------------------------------
+    SASS and Compass Compilation
+-------------------------------------------------------------------*/
+gulp.task('compass', function() {
+    return gulp.src(watched.scss)
+            .pipe(plumber())
+            .pipe(compass({
+                css: path.css,
+                sass: path.scss,
+                image: path.img
+            }))
+            .pipe(gulp.dest(path.css));
 });
 
-/*----------------------------------------------------------------------------
-*   Task compilation SASS (scss)
-*---------------------------------------------------------------------------*/
-gulp.task('compass', function(){
-    return gulp.src('public/dev/css/**/*.scss')
-        .pipe(compass({
-            css : 'public/assets/css/',
-            sass : 'public/dev/css/'
-        }))
-        .pipe(prefix("last 15 version"))
-        //.pipe(minifyCSS())
-        .pipe(gulp.dest('public/assets/css'));
-});
-
-
-
-
-/*----------------------------------------------------------------------------
-*   Task Watch
-*   - Surveille et compile les coffescript
-*   - Surveille et compile les scss
-*   - Lance liveReload lors des modifications
-*---------------------------------------------------------------------------*/
-gulp.task('watch', ['coffee', 'compass'], function(){
+/*-------------------------------------------------------------------
+    dev
+-------------------------------------------------------------------*/
+gulp.task('watch', function () {
     var server = livereload();
-    // coffeescript
-    gulp.watch(paths.dev.coffee, ['coffee']).on('change', function(event){
-        console.log('Fichier ' + event.path + 'a ete modofie');
+
+    gulp.watch(watched.coffee, ['coffee']).on('change', function(e){
+        console.log('Your coffee file' + e.path + ' has been compiled');
     });
-    // scss
-    gulp.watch(paths.dev.scss, ['compass']).on('change', function(event){
-        console.log('Fichier ' + event.path + 'a ete modofie');
+
+    gulp.watch(watched.scss, ['compass']).on('change', function(e){
+        console.log('Your scss file ' + e.path + ' has been compiled');
     });
-    // LiveReload
-    gulp.watch('./**').on('change', function(file) {
-        server.changed(file.path);
-        console.log('File change livereload');
+
+    gulp.watch(watched.all).on('change', function(e){
+        server.changed(e.path);
     });
+
+});
+
+
+/*------PROD TASKS------*/
+
+/*-------------------------------------------------------------------
+    Optimise images
+-------------------------------------------------------------------*/
+gulp.task('img', function() {
+  return gulp.src(watched.img)
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
+
+/*-------------------------------------------------------------------
+    Clean dist folder
+-------------------------------------------------------------------*/
+gulp.task('clean', function() {
+  return gulp.src('dist', {read: false}).pipe(clean());
+});
+
+/*-------------------------------------------------------------------
+    Prod
+-------------------------------------------------------------------*/
+gulp.task('prod', ['coffee', 'compass', 'clean', 'img'], function () {
+    return gulp.src(watched.html)
+        .pipe(useref.assets())
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe(minifyCss())
+        .pipe(cssFilter.restore())
+        .pipe(useref.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+/*-------------------------------------------------------------------
+    Zip production folder
+-------------------------------------------------------------------*/
+gulp.task('prod-zip', ['prod'], function () {
+    return gulp.src('dist/**/*')
+        .pipe(zip('dist.zip'))
+        .pipe(gulp.dest('.'));
+});
+
+/*-------------------------------------------------------------------
+    deploy-ftp
+-------------------------------------------------------------------*/
+gulp.task('deploy-ftp', ['prod'], function () {
+    return gulp.src('dist/**/*')
+        .pipe(ftp({
+            host: deploy.host,
+            user: deploy.user,
+            pass: deploy.pass
+        }));
+});
+
+/*-------------------------------------------------------------------
+    deploy-sftp
+-------------------------------------------------------------------*/
+gulp.task('deploy-sftp', ['prod'], function () {
+    return gulp.src('dist/**/*')
+        .pipe(sftp({
+            host: deploy.host,
+            user: deploy.user,
+            pass: deploy.pass
+        }));
 });
